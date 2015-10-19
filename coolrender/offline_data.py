a0b7b05fc423bbc3ff6fe771010548d6bafaa8b6
Kazutomo Yoshii <kazutomo.yoshii@gmail.com>
2015-10-18 21:08:13 -0500

    wip

--- coolrender/offline_data.py
+++ coolrender/offline_data.py
@@ -11,13 +11,11 @@
 import os, sys, time, re
 import json
 import numpy as np
+import math
 
 class reconstruct:
 
-    def __init__(self, cfgfn, jsonfn):
-        with open(cfgfn) as f:
-            self.cfg = json.load(f)
-
+    def __init__(self, jsonfn):
         # key is nodename
         self.info = {}
         self.data = {}
@@ -46,16 +44,6 @@ class reconstruct:
             if not self.data.has_key(k):
                 raise KeyError
 
-    def gettimerange(self):
-        t0 = []
-        t1 = []
-        for k in self.data.keys():
-            for k2 in self.data[k].keys():
-                a = self.data[k][k2]
-                t0.append(a[0]["time"])
-                t1.append(a[-1]["time"])
-        return np.max(t0), np.min(t1)
-
     def getnodes(self):
         n = []
         for k in self.data.keys():
@@ -67,17 +55,70 @@ class reconstruct:
         for k in self.data[nname].keys():
             s.append(k)
         return s
+    
+    def gettimerange(self):
+        t0 = [] # start times
+        t1 = [] # end times
+        for k in self.data.keys():
+            for k2 in self.data[k].keys():
+                a = self.data[k][k2]
+                t0.append(a[0]["time"])
+                t1.append(a[-1]["time"])
+        return np.max(t0), np.min(t1)
 
+    # nth frame start time. call after setfps()
+    def getnthstart(self,nth):
+        v = self.ts + nth*self.interval
+        return math.fmod(v, self.interval)
+    
+    def setfps(self,fps):
+        self.ts, self.te = self.gettimerange()
+        self.fps = fps
+        self.interval = 1.0/fps
+        self.nframes =  int((te-ts)/self.interval) - 1
+        # index for each samples. (may not be used)
+        self.sampleidx = {}
+        for n in self.data.keys():
+            self.sampleidx[n] = {}
+            for s in self.data[n].keys():
+                self.sampleidx[n][s] = -1
+                data = self.data[n][s]
+                for idx in range(len(data)):
+                    t = data[idx]["time"]
+                    if t >= self.ts and t < self.ts+self.interval:
+                        self.sampleidx[n][s] = idx
+                        break
+
+    def gettime2frameno(self,t):
+        n = int((t-self.ts)/self.interval)
+        return n
+                    
+    def getlist(self,nname,sname):
+        ret = [None for i in range(self.nframes)]
+        data = self.data[nname][sname]
+        for idx in range(len(data)):
+            t = data[idx]["time"]
+            fno = self.gettime2frameno(t)
+            if fno >= 0 and fno < self.nframes:
+                ret[fno] = data[idx]
+
+        return ret
+
+    
 if __name__ == "__main__":
 
-    r = reconstruct("chameleon.cfg", "chameleon.json" )
+    r = reconstruct("chameleon.json")
 
     nodes = r.getnodes()
     print nodes
-    samples = r.getsamples(nodes[0])
-    print samples
-    t1,t2 = r.gettimerange()
 
+    ts,te = r.gettimerange()
 
-        
+    r.setfps(4)
 
+    l = r.getlist(nodes[0], 'temp')
+    for i in l:
+        if i == None:
+            print 'na'
+        else:
+            print i['time']
