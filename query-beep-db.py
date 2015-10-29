8dd29d7bddba52feae578dcb42876492eed3f6e6
Kazutomo Yoshii <kazutomo.yoshii@gmail.com>
2015-10-29 03:09:34 +0000

    added options

--- query-beep-db.py
+++ query-beep-db.py
@@ -7,24 +7,84 @@
 import sys, os, time
 import sqlite3 as lite
 import json
+import zlib, base64
+import getopt
+
 
 # default: retrieve only data inserted within 3 secs
-nsec = 3
-oldt = time.time() - nsec
+secago = 3
+dbfile = '/dev/shm/node_power.sql'
+gtidx=-1
+compress=False
+last=-1
 
-if len(sys.argv) > 1:
-    oldt = float(sys.argv[1])
+def usage():
+    print ''
+    print 'Usage: query-beep-db.py [options]'
+    print ''
+    print '[options]'
+    print ''
+    print '--dbfile=filename : specify dbfile  (default=%s)' % dbfile
+    print '--secago=N : retrieve data between N sec old and now (default=%d)' % secago
+    print '--gtidx=N : retrieve data whose idx is greather than N (no default)'
+    print '--last=N : retrieve last N data (no default)   higher priority than --gtidx'
+    print '--compress : data compressed by zlib and print base64 encoded string'
+    print
 
-con = lite.connect('/dev/shm/node_power.sql')
+shortopt = "h"
+longopt = ['dbfile=', 'secago=', 'gtidx=', 'last=', 'compress']
+try:
+    opts, args = getopt.getopt(sys.argv[1:],
+                               shortopt, longopt)
+except getopt.GetoptError, err:
+    print err
+    usage()
+    sys.exit(1)
+
+for o, a in opts:
+    if o in ('-h'):
+        usage()
+        sys.exit(0)
+    elif o in ("--dbfile"):
+        dbfile=a
+    elif o in ("--secago"):
+        secago=float(a)
+    elif o in ("--gtidx"):
+        gtidx=int(a)
+    elif o in ("--compress"):
+        compress=True
+    elif o in ("--last"):
+        last=int(a)
+
+oldt = time.time() - secago
+
+con = lite.connect(dbfile)
 
 with con:
     cur = con.cursor()
 
-    cur.execute("SELECT * FROM Data Where Time > %lf ORDER BY TIME;" % oldt)
+    if last > 0:
+        cur.execute("SELECT * FROM Data Limit %d;" % last)
+    elif gtidx > 0 :
+        cur.execute("SELECT * FROM Data Where Id > %d Order by Id;" % gtidx)
+    else:
+        cur.execute("SELECT * FROM Data Where Time > %lf ORDER BY TIME;" % oldt)
 
     rows = cur.fetchall()
 
+    buf = ''
     for row in rows:
-        print row[2]
+        j = json.loads(row[2])
+        # add dbid so that the acquisition side can keep track
+        j['dbid'] = int(row[0])
+        buf += json.dumps(j)
+        buf += '\n'
+    if compress:
+        print base64.b64encode(zlib.compress(buf,9))
+    else:
+        print buf
+
+#   print len(buf)
+
 
 sys.exit(0)
